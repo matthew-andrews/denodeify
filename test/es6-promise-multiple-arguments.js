ec5ab5dae12b75f932e61dc17d6a8c987cb4a2a4
Matthew Andrews <matt@mattandre.ws>
2014-09-13 12:06:25 +0100

    Typo

---
+++ test/es6-promise-multiple-arguments.js
@@ -0,0 +1,24 @@
+require('es6-promise/dist/commonjs/promise/polyfill').polyfill();
+
+var denodeify = require("../");
+var assert = require('assert');
+
+function myNodeStyleFunction(callback) {
+	callback(null, 'a', 'b');
+}
+
+function myFilter(err, a, b) {
+	return [err, [a, b]];
+}
+
+describe('denodeify with es6 promise using multiple arguments', function(){
+	it('should pass multiple arguments to the next then', function(done) {
+		var myDenodeifiedNodeStyleFunction = denodeify(myNodeStyleFunction, myFilter);
+		myDenodeifiedNodeStyleFunction()
+			.then(function(results) {
+				assert.equal(results[0], 'a');
+				assert.equal(results[1], 'b');
+				done();
+			});
+	});
+});
