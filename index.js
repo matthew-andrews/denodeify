f63ef4705dd70adeb92f2640eaa30e8cb4cb916d
Matthew Andrews <matt@mattandre.ws>
2014-09-17 04:28:18 +0200

    Follow @petkaantonov's advice and try not to leak arguments

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = function(fun, filter) {
 	return function() {
-		var args = Array.prototype.slice.call(arguments, 0);
+
+		// Don't leak arguments
+		// https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#32-leaking-arguments
+		var args = new Array(arguments.length);
+		for (var i = 0; i < args.length; ++i) {
+			args[i] = arguments[i];
+		}
+
 		return new Promise(function(resolve, reject) {
 			args.push(function(err) {
 				var args = Array.prototype.slice.call(arguments, 0);
