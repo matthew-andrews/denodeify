5f7bddc5eb7877d1834d6ff2a69baff7b5300fc7
notgiorgi <notgiorgi@gmail.com>
2016-08-22 01:00:33 +0400

    Removed for loops, fancier syntax

--- index.js
+++ index.js
@@ -5,19 +5,11 @@
 
 		return function() {
 			var self = this;
-			var functionArguments = new Array(arguments.length + 1);
-
-			for (var i = 0; i < arguments.length; i += 1) {
-				functionArguments[i] = arguments[i];
-			}
+			var functionArguments = [].concat(Array.prototype.slice.call(arguments), [undefined]);
 
 			function promiseHandler(resolve, reject) {
 				function callbackFunction() {
-					var args = new Array(arguments.length);
-
-					for (var i = 0; i < args.length; i += 1) {
-						args[i] = arguments[i];
-					}
+					var args = Array.prototype.slice.call(arguments);
 
 					if (filter) {
 						args = filter.apply(self, args);
